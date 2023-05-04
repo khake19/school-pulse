96debfe96a60805ee6b0c87fa624ce7416118958
khake19 <draudekrek@gmail.com>
2023-05-05 01:05:24 +0800

    Clean up Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,17 @@
-import { Box, Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import Header from '~/components/Header'
 import Sidebar from '~/components/Sidebar'
 
-const Layout = () => {
+type Props = {
+  children: JSX.Element
+}
+
+const Layout = ({ children }: Props) => {
   return (
     <Grid templateAreas={`"header header" "nav main"`} gridTemplateRows={'80px 1fr'} gridTemplateColumns={'150px 1fr'}>
       <Header />
       <Sidebar />
-      <GridItem area={'main'}>
-        <Box>Main</Box>
-      </GridItem>
+      <GridItem area={'main'}>{children}</GridItem>
     </Grid>
   )
 }
