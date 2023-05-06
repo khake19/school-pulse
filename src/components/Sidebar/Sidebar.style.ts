615c273ec6a87b77a7a6c84c406b4af7a5734beb
khake19 <draudekrek@gmail.com>
2023-05-06 11:13:35 +0800

    Initial commit for sidebar

--- src/components/Sidebar/Sidebar.style.ts
+++ src/components/Sidebar/Sidebar.style.ts
@@ -2,7 +2,18 @@ import { css } from '@emotion/react'
 
 const SidebarStyle = {
   sidebar: css`
-    padding: 8px;
+    pos: sticky;
+    left: 5;
+    height: calc(100vh - 80px);
+    box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
+    flex-direction: column;
+    justify-content: space-between;
+  `,
+  avatar: css`
+    padding: 5%;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 4;
   `
 }
 
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import { Box, GridItem } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Avatar, Divider, Heading, IconButton, Flex, GridItem, Text } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
 import SidebarStyle from './Sidebar.style'
 
 const Sidebar = () => {
+  const [navSize, changeNavSize] = useState('large')
   return (
-    <GridItem bg="purple.50" area={'nav'} height="calc(100vh - 75px)">
-      <Box css={SidebarStyle.sidebar}>Nav</Box>
+    <GridItem area={'nav'}>
+      <Flex
+        css={SidebarStyle.sidebar}
+        width={navSize === 'small' ? '75px' : '200px'}
+        borderRadius={navSize === 'small' ? '0 15px 15px 0' : '0 30px 30px 0'}
+      >
+        <Flex p="5%" flexDir="column" alignItems={navSize === 'small' ? 'center' : 'flex-start'} as="nav">
+          <IconButton
+            aria-label="icon-menu"
+            background="none"
+            mt={5}
+            _hover={{ background: 'none' }}
+            icon={<FiMenu />}
+            onClick={() => {
+              if (navSize === 'small') {
+                changeNavSize('large')
+              } else {
+                changeNavSize('small')
+              }
+            }}
+          />
+        </Flex>
+        <Flex css={SidebarStyle.avatar} alignItems={navSize === 'small' ? 'center' : 'flex-start'}>
+          <Divider display={navSize === 'small' ? 'none' : 'flex'} />
+          <Flex mt={4} align="center">
+            <Avatar size="sm" />
+            <Flex flexDir="column" ml={4} display={navSize === 'small' ? 'none' : 'flex'}>
+              <Heading as="h3" size="sm">
+                Eduard Jazul
+              </Heading>
+              <Text color="gray">Admin</Text>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
     </GridItem>
   )
 }
