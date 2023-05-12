e92e3771fa28abfd523e3a572b1145bf497b8d41
khake19 <draudekrek@gmail.com>
2023-05-12 21:58:08 +0800

    Add teachers list

---
+++ src/app/teachers/Teacher.style.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react'
+
+const TeachersStyle = {
+  main: css`
+    box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
+    background-color: #ffff;
+    border-radius: 20px;
+    width: 20%;
+    margin: 12px;
+  `,
+  header: css`
+    margin: 20px 20px;
+  `,
+  avatar: css`
+    padding: 5%;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 4;
+  `
+}
+
+export default TeachersStyle
--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -1,11 +1,42 @@
 'use client'
-import { Box } from '@chakra-ui/react'
+import { Avatar, Box, Heading, List, ListItem, Text } from '@chakra-ui/react'
 import Layout from '~/components/Layout'
+import TeachersStyle from './Teacher.style'
 
 const Teachers = () => {
   return (
     <Layout>
-      <Box width="100%">Teachers</Box>
+      <Box css={TeachersStyle.main}>
+        <Box css={TeachersStyle.header}>
+          <Heading as="h4" size="md">
+            Teachers
+          </Heading>
+        </Box>
+        <Box>
+          <List>
+            <ListItem
+              display="flex"
+              alignItems="center"
+              height="60px"
+              padding="0 20px"
+              borderBottom="1px"
+              borderColor="gray.200"
+              mt={2}
+              _hover={{ bg: '#B9EDDD' }}
+            >
+              <Avatar size="md" src="https://robohash.org/sam" mr={2} />
+              <Box>
+                <Text fontSize="sm" fontWeight="600">
+                  Samantha Jazul
+                </Text>
+                <Text fontSize="xs" color="gray.500">
+                  Teacher 1
+                </Text>
+              </Box>
+            </ListItem>
+          </List>
+        </Box>
+      </Box>
     </Layout>
   )
 }
