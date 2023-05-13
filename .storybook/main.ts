418f0989d25d1eed37381544e0c8ecd64ef561ee
khake19 <draudekrek@gmail.com>
2023-05-13 09:17:04 +0800

    Fix alias in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,18 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
+
 const config: StorybookConfig = {
+  webpackFinal: async (config) => {
+    /**
+     * Add support for alias-imports
+     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
+     */
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      '~': [path.resolve(__dirname, '../src/'), path.resolve(__dirname, '../')],
+    };
+    return config;
+  },
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   staticDirs: ['../public'],
@@ -9,6 +22,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
-  }
+  },
 }
 export default config
