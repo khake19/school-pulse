5163a2af427d08c3208bdc2c5ae5da41f12f2497
Eduard Jazul <eduardjazul@Administrators-MacBook-Pro.local>
2023-08-28 10:20:17 +0800

    Separate Teacher list component from page

---
+++ src/app/teachers/TeacherList.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { Avatar, Box, Heading, List, ListItem, Text } from '@chakra-ui/react'
+import Layout from '~/components/Layout'
+import TeachersStyle from './Teacher.style'
+import useGetTeachers from './hooks/useGetTeachers'
+
+const TeacherList = () => {
+  const { teachers } = useGetTeachers()
+
+  return (
+    <Layout>
+      <Box css={TeachersStyle.main}>
+        <Box css={TeachersStyle.header}>
+          <Heading as="h4" size="md">
+            Teachers
+          </Heading>
+        </Box>
+        <Box>
+          <List>
+            {teachers?.data.map((teacher) => {
+              return (
+                <ListItem
+                  key={teacher.id}
+                  display="flex"
+                  alignItems="center"
+                  height="60px"
+                  padding="0 20px"
+                  borderColor="gray.200"
+                  mt={2}
+                  _hover={{ bg: '#B9EDDD' }}
+                >
+                  <Avatar size="md" src="https://robohash.org/sam" mr={2} />
+                  <Box>
+                    <Text fontSize="sm" fontWeight="600">
+                      {teacher.first_name} {teacher.last_name}
+                    </Text>
+                    <Text fontSize="xs" color="gray.500">
+                      Teacher 1
+                    </Text>
+                  </Box>
+                </ListItem>
+              )
+            })}
+          </List>
+        </Box>
+      </Box>
+    </Layout>
+  )
+}
+
+export default TeacherList
--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -1,62 +1,11 @@
-'use client'
-import { Avatar, Box, Heading, List, ListItem, Text } from '@chakra-ui/react'
-import Layout from '~/components/Layout'
-import TeachersStyle from './Teacher.style'
+import { Suspense } from 'react'
+import TeacherList from './TeacherList'
 
 const Teachers = () => {
   return (
-    <Layout>
-      <Box css={TeachersStyle.main}>
-        <Box css={TeachersStyle.header}>
-          <Heading as="h4" size="md">
-            Teachers
-          </Heading>
-        </Box>
-        <Box>
-          <List>
-            <ListItem
-              display="flex"
-              alignItems="center"
-              height="60px"
-              padding="0 20px"
-              borderBottom="1px"
-              borderColor="gray.200"
-              mt={2}
-              _hover={{ bg: '#B9EDDD' }}
-            >
-              <Avatar size="md" src="https://robohash.org/sam" mr={2} />
-              <Box>
-                <Text fontSize="sm" fontWeight="600">
-                  Samantha Jazul
-                </Text>
-                <Text fontSize="xs" color="gray.500">
-                  Teacher 1
-                </Text>
-              </Box>
-            </ListItem>
-            <ListItem
-              display="flex"
-              alignItems="center"
-              height="60px"
-              padding="0 20px"
-              borderBottom="1px"
-              borderColor="gray.200"
-              _hover={{ bg: '#B9EDDD' }}
-            >
-              <Avatar size="md" src="https://robohash.org/callum" mr={2} />
-              <Box>
-                <Text fontSize="sm" fontWeight="600">
-                  Callum Kerk Jazul
-                </Text>
-                <Text fontSize="xs" color="gray.500">
-                  Teacher 2
-                </Text>
-              </Box>
-            </ListItem>
-          </List>
-        </Box>
-      </Box>
-    </Layout>
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>loading... on initial request</p>}>
+      <TeacherList />
+    </Suspense>
   )
 }
 
