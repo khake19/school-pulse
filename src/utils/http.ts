8791d9df331b1aad10edcdc2197f0af51225dc0e
khake19 <kerk@barbooksaustralia.com>
2023-11-19 10:15:41 +0800

    Move defaultOptions to http method

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -3,18 +3,18 @@ const defaultOptions = {
 }
 
 const http = async <T>(endpoint: string, options: object = {}): Promise<T> => {
+  const response: Response = await fetch(endpoint, {...defaultOptions, ...options})
   if (!response.ok) throw new Error(response.statusText)
   return await response.json()
 }
 
 const get = async <T>(endpoint: string, options: object = {}): Promise<T> => {
-  const response = await http<T>(endpoint, { ...defaultOptions, ...options })
+  const response = await http<T>(endpoint, options)
   return response
 }
 
 const post = async <T, P extends object>(endpoint: string, params: P): Promise<T> => {
   const options = {
-    ...defaultOptions,
     method: 'post',
     body: JSON.stringify(params)
   }
