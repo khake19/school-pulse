a0a6d6b9eb3bbc34d286e01fb851c12042777dee
Eduard Jazul <eduardjazul@Admins-MacBook-Pro.local>
2024-04-07 20:19:08 +0800

    Initial commit for Pagination

--- src/components/Table/Pagination/Pagination.tsx
+++ src/components/Table/Pagination/Pagination.tsx
@@ -1,45 +1,44 @@
 import { useState } from 'react'
 import ReactPaginate from 'react-paginate'
-import { chakra } from "@chakra-ui/react"
-
+import { chakra } from '@chakra-ui/react'
 
 const StyledPagination = chakra(ReactPaginate, {
-    baseStyle: {
-        margin: '5px auto',
-        display: 'flex',
-        listStyle: 'none',
+  baseStyle: {
+    margin: '5px auto',
+    display: 'flex',
+    listStyle: 'none',
+    outline: 'none',
+    flexDirection: 'row',
+    padding: '0 5rem',
+    li: {
+      marginRight: '5px',
+      a: {
+        borderRadius: '0.2rem',
+        padding: '0.5rem 0.9rem',
+        border: 'solid 1px #f0f0f0',
+        cursor: 'pointer',
         outline: 'none',
-        flexDirection: 'row',
-        padding: '0 5rem',
-        li: {
-            marginRight: '5px',
-            a: {
-                borderRadius: '0.2rem',
-                padding: '0.5rem 0.9rem',
-                border: 'solid 1px #f0f0f0',
-                cursor: 'pointer',
-                outline: 'none',
-                backgrondColor: '#47ccde',
-                color: 'darken(#f0f0f0, 45%)',
-                textDecoration: 'none',
-                transition: 'all 0.3s ease',
-                display: 'inline-block'
-            },
-            "&:hover": {
-                backgroundColor: '#1d1f20',
-                border: 'solid 1px #1d1f20',
-                color: 'white',
-            }
-         },
-         ".active": {
-            a: {
-                backgroundColor: '#1d1f20',
-                border: 'solid 1px #1d1f20',
-                color: 'white',
-            }
-         }
-        },
-  })
+        backgrondColor: '#47ccde',
+        color: 'darken(#f0f0f0, 45%)',
+        textDecoration: 'none',
+        transition: 'all 0.3s ease',
+        display: 'inline-block'
+      },
+      '&:hover': {
+        backgroundColor: '#1d1f20',
+        border: 'solid 1px #1d1f20',
+        color: 'white'
+      }
+    },
+    '.active': {
+      a: {
+        backgroundColor: '#1d1f20',
+        border: 'solid 1px #1d1f20',
+        color: 'white'
+      }
+    }
+  }
+})
 const Pagination = () => {
   const [page, setPage] = useState(0)
   const pageCount = 10
@@ -48,24 +47,24 @@ const Pagination = () => {
 
   return (
     <StyledPagination
-    nextLabel="next"
-    onPageChange={handlePageClick}
-    pageRangeDisplayed={3}
-    marginPagesDisplayed={2}
-    pageCount={pageCount}
-    previousLabel="previous"
-    pageClassName="page-item"
-    pageLinkClassName="page-link"
-    previousClassName="page-item"
-    previousLinkClassName="page-link"
-    nextClassName="page-item"
-    nextLinkClassName="page-link"
-    breakLabel="..."
-    breakClassName="page-item"
-    breakLinkClassName="page-link"
-    containerClassName="pagination"
-    activeClassName="active"
-    renderOnZeroPageCount={null}
+      nextLabel="next"
+      onPageChange={handlePageClick}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={2}
+      pageCount={pageCount}
+      previousLabel="previous"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      previousClassName="page-item"
+      previousLinkClassName="page-link"
+      nextClassName="page-item"
+      nextLinkClassName="page-link"
+      breakLabel="..."
+      breakClassName="page-item"
+      breakLinkClassName="page-link"
+      containerClassName="pagination"
+      activeClassName="active"
+      renderOnZeroPageCount={null}
     />
   )
 }
