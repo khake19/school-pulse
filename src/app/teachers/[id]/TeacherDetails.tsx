c78e17607af35fc476544cae885790fada5a5a11
Eduard Jazul <eduardjazul@Admins-MacBook-Pro.local>
2024-04-17 21:07:01 +0800

    use chakra tab

--- src/app/teachers/[id]/TeacherDetails.tsx
+++ src/app/teachers/[id]/TeacherDetails.tsx
@@ -1,27 +1,26 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import TeachersStyle from '../Teacher.style'
-import Tabs from '~/components/Tabs/Tabs'
 
 const TeacherDetails = () => {
   return (
     <Box css={TeachersStyle.main}>
-      <Tabs>
-        <Tabs.TabList>
-          <Tabs.Tab>One</Tabs.Tab>
-          <Tabs.Tab>Two</Tabs.Tab>
-          <Tabs.Tab>Three</Tabs.Tab>
-        </Tabs.TabList>
-        <Tabs.TabPanels>
-          <Tabs.Panel>
-            <p>Tab one content!</p>
-          </Tabs.Panel>
-          <Tabs.Panel>
-            <p>Tab two content!</p>
-          </Tabs.Panel>
-          <Tabs.Panel>
-            <p>Tab three content!</p>
-          </Tabs.Panel>
-        </Tabs.TabPanels>
+      <Tabs position="relative">
+        <TabList>
+          <Tab>Profile</Tab>
+          <Tab>Documents</Tab>
+          <Tab>Settings</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p> 
+          </TabPanel>
+          <TabPanel>
+            <p>twos!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
       </Tabs>
     </Box>
   )
