63c7622a74c7071d4878d077507067a4d01e65f9
Eduard Jazul <kerk@barbooksaustralia.com>
2025-01-08 21:07:58 +0800

    refactor login module

--- src/app/login/hooks/useLogin.ts
+++ src/app/login/hooks/useLogin.ts
@@ -1,22 +1,29 @@
 import { useMutation } from '@tanstack/react-query'
 import { redirect } from 'next/navigation'
+import useAlert from '~/hooks/useAlert'
 
 import loginService from '../services/login.service'
 import { ILoginParams } from '../types/login'
+import getErrorMessage from '~/utils/error'
 
 const useLogin = (options?: object) => {
-  const { mutateAsync, isLoading: isLoggingIn, isError, isSuccess } = useMutation(loginService.login, options)
+  const alert = useAlert()
+  const { mutateAsync, isSuccess } = useMutation(loginService.login, options)
   const login = async (params: ILoginParams) => {
     try {
       await mutateAsync(params)
-    } catch (error) {}
+    } catch (error) {
+      if (error instanceof Error) {
+        alert.fetchError(getErrorMessage(error))
+      }
+    }
   }
 
   if (isSuccess) {
     redirect('/')
   }
 
-  return { login, isLoggingIn, isError, isSuccess }
+  return { login }
 }
 
 export default useLogin
